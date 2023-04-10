2bd6f7a0cd9ffec4cc73ce267bbc785818433e29
lincoln gibson <lincolngibson7@gmail.com>
2023-04-10 19:51:02 -0300

    Add javascript to dynamically generate project cards and removed hard coded html.

--- script.js
+++ script.js
@@ -23,55 +23,104 @@ navLinks.forEach((element) => {
 });
 
 
-// projects 
+// projects array
 
 const projects = [
 {
-  name: "Project 1",
+  name: "Multi-Post Stories Gain+Glory",
   description:"This is a brief description of the project",
   image:"./images/Indicator.png",
-  technologies:["HTML","CSS","JavaScript"],
+  technologies:["Ruby on Rails","JavaScript","CSS","HTML"],
   liveVersion:"https://www.example.com/",
   source:"https://www.github.com/example",
 },
 {
-  name: "Project 2",
+  name: "Multi-Post Stories Gain+Glory",
   description:"This is a brief description of the project",
   image:"./images/Indicator.png",
-  technologies:["HTML","CSS","JavaScript"],
+  technologies:["Ruby on Rails","JavaScript","CSS","HTML"],
    liveVersion:"https://www.example.com/",
   source:"https://www.github.com/example",
 },
   {
-  name: "Project 3",
+  name: "Multi-Post Stories Gain+Glory",
   description:"This is a brief description of the project",
   image:"./images/Indicator.png",
-  technologies:["HTML","CSS","JavaScript"],
+  technologies:["Ruby on Rails","JavaScript","CSS","HTML"],
   liveVersion:"https://www.example.com/",
   source:"https://www.github.com/example",
 },
   {
-  name: "Project 4",
+  name: "Multi-Post Stories Gain+Glory",
   description:"This is a brief description of the project",
   image:"./images/Indicator.png",
-  technologies:["HTML","CSS","JavaScript"],
+  technologies:["Ruby on Rails","JavaScript","CSS","HTML"],
   liveVersion:"https://www.example.com/",
   source:"https://www.github.com/example",
 },
   {
-  name: "Project 5",
+  name: "Project 5",name: "Multi-Post Stories Gain+Glory",
   description:"This is a brief description of the project",
   image:"./images/Indicator.png",
-  technologies:["HTML","CSS","JavaScript"],
-   liveVersion:"https://www.example.com/",
+  technologies:["Ruby on Rails","JavaScript","CSS","HTML"],
+  liveVersion:"https://www.example.com/",
   source:"https://www.github.com/example",
 },
   {
-  name: "Project 6",
+  name: "Multi-Post Stories Gain+Glory",
   description:"This is a brief description of the project",
   image:"./images/Indicator.png",
-  technologies:["HTML","CSS","JavaScript"],
+  technologies:["Ruby on Rails","JavaScript","CSS","HTML"],
   liveVersion:"https://www.example.com/",
   source:"https://www.github.com/example",
 }
 ]
+
+
+// generate projects
+
+  const worksCards = document.getElementById('works-cards');
+   console.log(worksCards)
+  
+  for(const project of projects){
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('text-container');
+
+    const titleContainer = document.createElement('title-container');
+    titleContainer.classList.add('title-container');
+
+    const projectTitle = document.createElement('h3');
+    projectTitle.classList.add('project-title');
+    projectTitle.textContent = project.name;
+
+    titleContainer.appendChild(projectTitle)
+
+    const techList = document.createElement('ul');
+    techList.classList.add('tech');
+
+    for(const tech of project.technologies){
+      const techItem = document.createElement('li');
+      techItem.textContent = tech;
+      techList.appendChild(techItem);
+    };
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('button-container');
+
+    const projectButton = document.createElement('button')
+    projectButton.classList.add('project-button');
+    projectButton.textContent = 'See project';
+
+
+    titleContainer.appendChild(projectTitle);
+    textContainer.appendChild(titleContainer);
+    textContainer.appendChild(techList);
+    buttonContainer.appendChild(projectButton);
+    textContainer.appendChild(buttonContainer);
+    card.appendChild(textContainer);
+
+    worksCards.appendChild(card);
+  };
